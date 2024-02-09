5a642685b1c7b7023a3eb8435980be1d0ccecdc2
john <jarrington08@gmail.com>
2024-02-09 00:28:11 -0600

    began adding the ability for users to add books to their personal collections. Need to make Google Books API call and test

---
+++ bsreact/src/users/AddBook.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+export default function AddBook() {
+
+const userId = window.sessionStorage.getItem("userId");
+const [books, setBooks] = useState([]);
+//Do I need to make this a book object?
+//Or would I select one book object from array of books?
+const [book, setBook] = useState('');
+const [searchTerm, setSearchTerm] = useState('');
+
+const handleInputChangeSearch = (e) => {
+    setSearchTerm(e.target.value);
+    //make GB api call here
+}
+
+useEffect(() => {
+
+    //if handleInputChangeSearch function makes GB api call is this necessary??
+    const fetchBooks = async () => {
+        //api call for onChange input
+    };
+
+    const addBook = async () => {
+        //api call for onChange input
+    };
+
+    fetchBooks();
+    addBook();
+
+    },[]);
+
+    return(
+        <body>
+            <input type="text" value={searchTerm} placeholder='Add a book' onChange={handleInputChangeSearch}/>
+        </body>
+    )
+
+
+}
