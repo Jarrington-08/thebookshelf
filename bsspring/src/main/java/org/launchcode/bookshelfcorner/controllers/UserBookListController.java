a93259dbd35313fdfff6bbb340e8d3309a594fc0
john <jarrington08@gmail.com>
2024-03-04 00:55:12 -0600

    debugged issue with series of null values. Realized that the first parameter was accepting all data as one JSON data, leaving the others null

--- bsspring/src/main/java/org/launchcode/bookshelfcorner/controllers/UserBookListController.java
+++ bsspring/src/main/java/org/launchcode/bookshelfcorner/controllers/UserBookListController.java
@@ -15,9 +15,11 @@ public class UserBookListController {
     BookRepository bookRepository;
 
     @PostMapping("/addBook/{userId}")
+    //This is treating the JSON object as the "title" parameter and treating the others as null. Why? Do I need to declare a different data type besides String? How do I package it in Java? Need to research POST JSON objects
+    //Do I need to use a book object? Don't forget to account for missing data (again, do I send default values from front end if missing?)
     public String addBook(@PathVariable int userId, @RequestBody String title, String author, Integer ISBN, Integer yearPublished, String coverUrl) {
 //        String a = valueOf(yearPublished);
-        System.out.println(title + author + ISBN + yearPublished + coverUrl);
+        System.out.println(title);
         return "Book added";
     }
 }
