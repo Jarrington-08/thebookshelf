e086ffed53553ac6890b5b0f0c0cfbc9eacd16fe
john <jarrington08@gmail.com>
2024-03-12 01:42:26 -0500

    Added better logic to addBook controller. I need to refactor and streamline however. I've decided to allow duplicate books to be uploaded to a user list in case they own multiple copies. I added a findByIsbn method to bookRepository to ensure we are working with identical volumes. I need to implement this when I check if a book exists in the repo before saving it.

--- bsspring/src/main/java/org/launchcode/bookshelfcorner/controllers/UserBookListController.java
+++ bsspring/src/main/java/org/launchcode/bookshelfcorner/controllers/UserBookListController.java
@@ -29,26 +29,36 @@ public class UserBookListController {
     //Do I need to use a book object? Don't forget to account for missing data (again, do I send default values from front end if missing?)
     //Do I just default to null? And then handle it again in the front end when nothing renders for that data?
     //Needs to return EntityResponse ultimately
-    //Not sure if declaring a newBook variable is necessary. Can we just save the value passed to the function? Need to test this
     public String addBook(@PathVariable int userId, @RequestBody Book book) {
-        Book newBook = new Book(book.getTitle(), book.getAuthors(), book.getIsbn(), book.getYearPublished(), book.getCoverUrl());
-//        System.out.println(newBook.getAuthors());
-//        System.out.println(newBook.getIsbn());
-//        System.out.println(newBook.getYearPublished());
-//        System.out.println(newBook.getCoverUrl());
+        //I want  to refactor all this logic. 1. If user not present return error message 2. If book already saved in repo, then add user
+        // to user list and add book to user bookList (regardless of whether it exists in user's list i.e. multiple copies of same book are possible
+        //In future, may add a confirmation box with buttons to confirm adding duplicate
+        //If book not in repo then add user and save to repo and add book to repo
+        //Need to test what happens when a second user uploads same book. It should just add them to List user in book object. Manually??
+        Optional<Book> optNewBook = bookRepository.findByIsbn(book.getIsbn());
+        if (optNewBook.isPresent()){
+            Book existingBook = optNewBook.get();
+            Optional<User> optUser = userRepository.findById(userId);
+            if (optUser.isPresent()) {
+                User user = optUser.get();
+                if (user.getBookList().contains(existingBook)) {
+                    //This is where I need ro return HTTPresponse with message "Book already added"
+                } else {
+                    user.addBook(existingBook);
+                    existingBook.addUser(user);
+                }
+        }
+        book.setYearPublished(Integer.valueOf(book.getYearPublished().toString().substring(0,4)));
+//        Book newBook = new Book(book.getTitle(), book.getAuthors(), book.getIsbn(), book.YearPublished(), book.getCoverUrl());
 
-        Optional<User> optUser = userRepository.findById(userId);
-        if (optUser.isPresent()) {
-            User user = optUser.get();
-            user.addBook(newBook);
-        } else { }
-        bookRepository.save(newBook);
+
+        }
+
+        bookRepository.save(book);
             //This is why I need to refactor all API methods to return HttpResponse - Can't return a String
-            //return "User not found";
-        //Need to figure out why ArrayList is saved as "BLOB" in MySQL and how to work with it / fix it
-        //Do I need to add book to user booklist manually? Does hibernate do that for me?
-        //Do I need to add user to Book users? Does hibernate do that for me?
-        //Seems like bookRequestDTO is unecessary because it won't reduce remote calls as far as I can see. DTO's seem to be intended for bundling data to reduce remote calls / method calls
+            //return "User not found"
+            //Seems like bookRequestDTO is unecessary because it won't reduce remote calls as far as I can see. DTO's seem to be intended for bundling data to reduce remote calls / method calls
+            //Eventually when I add an Author object (i.e. to be able to sort lists by author I will probably use a DTO to bundle book and author separately)
         return "Book added";
     }
 
--- bsspring/src/main/java/org/launchcode/bookshelfcorner/repository/BookRepository.java
+++ bsspring/src/main/java/org/launchcode/bookshelfcorner/repository/BookRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 //What would a join table look like for many to many users to books?
 //Do we need just one book repo with ALL books?
 //Should users have their own book repo? NO Because the api call can request only books associated with userID
 @Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
-
+    Optional<Book> findByIsbn(long isbn);
 }
